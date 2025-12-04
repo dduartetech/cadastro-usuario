5b75331c6c694e752d5904bf02727a74fd37da98
Diego Duarte <diegocavalcante040307@gmail.com>
2025-12-04 16:08:45 -0300

    adicionada classes de security

---
+++ src/main/java/com/diegoduarte/usuario/infrastructure/entity/Usuario.java
@@ -0,0 +1,59 @@
+package com.diegoduarte.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY) //auto increment
+    private Long id;
+
+    @Column(name = "nome", length = 100)
+    private String nome;
+
+    @Column (name = "email", length = 100)
+    private String email;
+
+    @Column (name = "senha", length = 8)
+    private String senha;
+
+    @OneToMany(cascade = CascadeType.ALL) /* cascade = quando excluir um usuario as
+                                            outras tabelas (tel e end tb sumirão!) */
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Endereco> endereços;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private List<Telefone> telefones;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
